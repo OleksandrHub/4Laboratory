25fabd5b9be887b902beffc75d6c8b0a3f3d32ab
Oleksandr <sasha.klishch1@gmail.com>
2025-02-16 12:49:32 +0200

    Create variant1.cpp

---
+++ variant1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 2.1, S = 0;
+    int i, n;
+    printf("n = "); scanf("%i",&n);
+    for (i = 1; i <= n ; i++){
+        S += (a*a+i*i) / (2*i);
+    }
+    printf("%f", S);
+    return 0;
+}
